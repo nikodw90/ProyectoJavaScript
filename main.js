5263affd2406750ad9f4be5175499e28fe110f5a
Niko <nicolas.tapia.meza@gmail.com>
2021-05-19 01:53:58 -0400

    se actualizo la pagina

--- main.js
+++ main.js
@@ -12,19 +12,48 @@ if(Guardado === null){
   carrito = [Guardado]
 }
 
+
+// const templadeCard = document.getElementsById('templade-cards').content
+// const fragmento = document.createDocumentFragment()
+
+
 document.addEventListener('DOMContentLoaded', () =>{
   fetchData()
 })
 
 const fetchData = async () =>{
   try {
-    const res = await fetch('./basededatos.json')
+    const res = await fetch('https://raw.githubusercontent.com/nikodw90/ProyectoJavaScript/a089ac47064cdc1276def36dde7e66213eb7353d/basededatos.json')
     const data = await res.json()
-    console.log(data)
+    // console.log
+    pintarCards(data)
   } catch (error) {
     console.log(error)
   }
 } 
+  const contenedorProductos = document.querySelector('#contenedor-productos')
+  const pintarCards = (data) => {
+
+
+// crear las cards 
+
+  const template = document.querySelector('#templade-productos').content
+  const fragmento = document.createDocumentFragment()
+  console.log(template)
+  data.forEach(producto =>{
+    console.log(producto)
+    template.querySelector('h3').textContent = producto.Nombre
+    template.querySelector('p').textContent = producto.descripcion
+    template.querySelector('h4').textContent = producto.precio
+
+
+    const clone = template.cloneNode(true)
+    fragmento.appendChild(clone)
+  })
+  contenedorProductos.appendChild(fragmento)
+}
+
+// Funcion agregar carrito
 
 function agregar(precio) {
     carrito.push(precio);
@@ -47,6 +76,4 @@ function agregar(precio) {
     console.log(`tu cantidad es de ${carrito.length} en tu carrito` + " " + (precio))
     
     
-    
-}
-
+  }
