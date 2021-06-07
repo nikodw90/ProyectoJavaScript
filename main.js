cf154d6d5a2ce431291bcee4ba644df388cca818
Niko <nicolas.tapia.meza@gmail.com>
2021-06-06 22:03:23 -0400

    se agregaron cambios para el proyecto final

--- main.js
+++ main.js
@@ -1,79 +1,148 @@
-
-
+const Clickbutton = document.querySelectorAll('.button')
+const tbody = document.querySelector('.tbody')
 let carrito = []
-let acumuladorDeProdutos = []
 
-let Guardado = localStorage.ProductosAgregados;
-console.log(Guardado)
-if(Guardado === null){
-  alert('no hay productos agregados')
-  carrito = []
-}else{
-  carrito = [Guardado]
-}
-
-
-// const templadeCard = document.getElementsById('templade-cards').content
-// const fragmento = document.createDocumentFragment()
-
-
-document.addEventListener('DOMContentLoaded', () =>{
-  fetchData()
+Clickbutton.forEach(btn => {
+  btn.addEventListener('click', addToCarritoItem)
 })
 
-const fetchData = async () =>{
-  try {
-    const res = await fetch('https://raw.githubusercontent.com/nikodw90/ProyectoJavaScript/a089ac47064cdc1276def36dde7e66213eb7353d/basededatos.json')
-    const data = await res.json()
-    // console.log
-    pintarCards(data)
-  } catch (error) {
-    console.log(error)
+//traer informacion de los produtos
+function addToCarritoItem(e){
+  const button = e.target
+  const item = button.closest('.card')
+  const itemTitle = item.querySelector('.card-title').textContent;
+  const itemPrice = item.querySelector('.precio').textContent;
+  
+  
+  const newItem = {
+    title: itemTitle,
+    precio: itemPrice,
+    cantidad: 1
   }
-} 
-  const contenedorProductos = document.querySelector('#contenedor-productos')
-  const pintarCards = (data) => {
 
-
-// crear las cards 
-
-  const template = document.querySelector('#templade-productos').content
-  const fragmento = document.createDocumentFragment()
-  console.log(template)
-  data.forEach(producto =>{
-    console.log(producto)
-    template.querySelector('h3').textContent = producto.Nombre
-    template.querySelector('p').textContent = producto.descripcion
-    template.querySelector('h4').textContent = producto.precio
-
-
-    const clone = template.cloneNode(true)
-    fragmento.appendChild(clone)
-  })
-  contenedorProductos.appendChild(fragmento)
+  addItemCarrito(newItem)
 }
 
-// Funcion agregar carrito
 
-function agregar(precio) {
-    carrito.push(precio);
-    localStorage.setItem('ProductosAgregados', JSON.stringify(carrito))
+function addItemCarrito(newItem){
+// alert cuando se agrega un producto
+  const alert = document.querySelector('.alert')
+
+  setTimeout( function(){
+    alert.classList.add('hide')
+  }, 2000)
+    alert.classList.remove('hide')
     
-    $(".boton").click(function(){ 
-      $(".modal").show(200); 
-      
-    });
-    $(".modal .bodymodal button").click(function(){ 
-      $(".modal").hide(200); 
-    });
-    
-        
-    let ProductoAgregado = function(){
-        console.log(precio)
+  const InputElemnto = tbody.getElementsByClassName('input__elemento')
+
+  //funcion para que no se repita el producto en carrito de compras
+  for(let i =0; i < carrito.length ; i++){
+    if(carrito[i].title.trim() === newItem.title.trim()){
+      carrito[i].cantidad ++;
+      const inputValue = InputElemnto[i]
+      inputValue.value++;
+      CarritoTotal()
+      return null;
     }
-    
-    let boton = document.getElementById('.boton');addEventListener('click', ProductoAgregado)
-    console.log(`tu cantidad es de ${carrito.length} en tu carrito` + " " + (precio))
-    
-    
   }
+  
+  carrito.push(newItem)
+  
+  renderCarrito()
+} 
+
+//crear cards para la pesaña de el carrito
+
+function renderCarrito(){
+  tbody.innerHTML = ''
+  carrito.map(item => {
+    const tr = document.createElement('tr')
+    tr.classList.add('ItemCarrito')
+    const Content = `
+    
+    <th scope="row">1</th>
+            <td class="table__productos">
+             <h6 class="title">${item.title}</h6>
+            </td>
+            <td class="table__price"><p>${item.precio}</p></td>
+            <td class="table__cantidad">
+              <input type="number" min="1" value=${item.cantidad} class="input__elemento">
+              <button class="delete btn btn-danger">x</button>
+            </td>
+    
+    `
+    tr.innerHTML = Content;
+    tbody.append(tr)
+
+    tr.querySelector(".delete").addEventListener('click', removeItemCarrito)
+    tr.querySelector(".input__elemento").addEventListener('change', sumaCantidad)
+  })
+  CarritoTotal()
+}
+
+//funcion para sumar total de el carrito
+
+function CarritoTotal(){
+  let Total = 0;
+  const itemCartTotal = document.querySelector('.itemCartTotal')
+  carrito.forEach((item) => {
+    const precio = Number(item.precio.replace("$", ''))
+    Total = Total + precio*item.cantidad
+  })
+
+  itemCartTotal.innerHTML = `Total $${Total}`
+  addLocalStorage()
+}
+//funcion para remover productos de el carrito
+
+function removeItemCarrito(e){
+  const buttonDelete = e.target
+  const tr = buttonDelete.closest(".ItemCarrito")
+  const title = tr.querySelector('.title').textContent;
+  for(let i=0; i<carrito.length ; i++){
+
+    if(carrito[i].title.trim() === title.trim()){
+      carrito.splice(i, 1)
+    }
+  }
+
+//alert cuando se remueve el producto
+
+  const alert = document.querySelector('.remove')
+
+  setTimeout( function(){
+    alert.classList.add('remove')
+  }, 2000)
+    alert.classList.remove('remove')
+
+  tr.remove()
+  CarritoTotal()
+}
+
+//funcion para sumar cantidad en carrito total
+
+function sumaCantidad(e){
+  const sumaInput  = e.target
+  const tr = sumaInput.closest(".ItemCarrito")
+  const title = tr.querySelector('.title').textContent;
+  carrito.forEach(item => {
+    if(item.title.trim() === title){
+      sumaInput.value < 1 ?  (sumaInput.value = 1) : sumaInput.value;
+      item.cantidad = sumaInput.value;
+      CarritoTotal()
+    }
+  })
+}
+//funcion para guardar productos aunque se actualice la pagina web
+function addLocalStorage(){
+  localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+
+window.onload = function(){
+  const storage = JSON.parse(localStorage.getItem('carrito'));
+  if(storage){
+    carrito = storage;
+    renderCarrito()
+  }
+}
+
